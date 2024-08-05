c8c1f838c629c8a02cb554f8c95094dd159bba37
Andrés Vilá <vilaandres@gmail.com>
2024-08-05 12:36:33 -0500

    Added more chapters

--- src/config.js
+++ src/config.js
@@ -20,15 +20,15 @@ var config = {
     "CNMH",
   chapters: [
     {
-      id: "slug-style-id",
-      alignment: "left",
+      id: "cap01",
+      alignment: "right",
       hidden: false,
       title: "Bogota",
       image: "./images/jaimeG.png",
       description:
         "Lorem ipsum dolor sit amet consectetur adipiscing elit primis in, viverra eros lacus nulla aliquet ante nam consequat conubia, ultrices sagittis tristique fringilla cubilia pulvinar eu egestas. Fames neque massa senectus ultricies habitant in turpis nisi, hendrerit sem facilisis pulvinar auctor ridiculus phasellus, non luctus nibh risus habitasse purus congue. Felis ultricies urna velit morbi facilisi fringilla laoreet orci dignissim, egestas ut malesuada rhoncus eget nulla ultrices justo, class dapibus imperdiet litora leo bibendum lacus mus.",
       location: {
-        center: [-74.20084, 4.70324], //4.622786830213004, -74.07809809815039
+        center: [-74.03084, 4.70324], //4.622786830213004, -74.07809809815039
         zoom: 10,
         pitch: 45,
         bearing: 0,
@@ -51,7 +51,7 @@ var config = {
       ],
     },
     {
-      id: "second-identifier",
+      id: "cap02",
       alignment: "left",
       hidden: false,
       title: "MAL Crew - Mural Jaime Garzon",
@@ -84,7 +84,7 @@ var config = {
       ],
     },
     {
-      id: "second-identifier1",
+      id: "cap03",
       alignment: "right",
       hidden: false,
       title: "Mural Carrera 40",
@@ -110,7 +110,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      id: "third-identifier",
+      id: "cap04",
       alignment: "left",
       hidden: false,
       title: "Facultad de Diseño Grafico UNAL",
@@ -130,7 +130,7 @@ var config = {
       onChapterExit: [],
     },
     {
-      id: "fourth-chapter",
+      id: "cap05",
       alignment: "right",
       hidden: false,
       title: "Plaza Che",
@@ -139,6 +139,86 @@ var config = {
         "En el año 2024, durante el paro universitario adelantado por la exigencia de los estudiantes de avanzar en democracia interna en la universidad, y suscitado por el irrespeto a la consulta para la elección del rector, la plaza Ché y los símbolos que históricamente la dotaron de sentido vivieron una importante transformación. A las imágenes del Ché Guevara, presente desde inicios de los años noventa, y a la de Jaime Garzón, pintada desde el 2016, se sumaron las representaciones de Policarpa Salavarrieta, Jorge Eliecer Gaitán y la estudiante Beatriz Sandoval, quien murió en el marco de una protesta el 16 de mayo de 1991. Además, un látigo y una espada rotas, junto a la frase “con autonomía y sin permiso”, rematan una composición con significados libertarios y pacifistas. Esto ha reflejado la consolidación de la imagen de Jaime Garzón como uno de los principales símbolos de este lugar de memoria. Aunque en la Universidad Nacional su figura ha sido apropiada históricamente por muchos estudiantes, su rostro fue pintado en este lugar por primera vez en octubre de 2016, en medio de una discusión sobre los símbolos que representaban a la UN y su comunidad, y, particularmente, sobre la figura del Che. Sectores de la comunidad rechazaban e intentaban dañar el rostro del revolucionario argentino, mientras otros defendían su presencia e incluso la vinculaban a la memoria de los hermanos San Juan, autores de la primera versión de este mural y víctimas de desaparición forzada 7 . Entre tanto, el mismo grupo de estudiantes que había repintado el Che semanas atrás, se propuso pintar a Jaime Garzón búscando que en su figura se encontrara la unidad fragmentada entre los universitarios.",
       location: {
         center: [-74.08284071534263, 4.635688562295812], //4.635688562295812, -74.08284071534263
+        zoom: 19,
+        pitch: 70,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
+      id: "cap06",
+      alignment: "left",
+      hidden: false,
+      title: "Poste conmemorativo a Jaime Garzón",
+      image: "./images/poste.png",
+      description:
+        "En la madrugada del 13 de agosto de 1999, la camioneta de Jaime Garzón se estrelló contra este poste tras los disparos que acabaron con su vida. Desde entonces, este poste ha sido intervenido gráficamente en cada uno de los aniversarios del asesinato de Garzón por su hermana Marisol y con ayuda de múltiples colectivos artísticos como Dexpierte apostándole a la resignificación y apropiación del espacio público como mecanismo para la exigencia de justicia y verdad. Asimismo, estas intervenciones han estado acompañadas de acciones performativas, homenajes y procesos simbólicos para la activación colectiva de la memoria de Jaime Garzón, la dimensión política de su asesinato",
+      location: {
+        center: [-74.09080645582132, 4.632445600628698], //4.632445600628698, -74.09080645582132
+        zoom: 19,
+        pitch: 70,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
+      id: "cap07",
+      alignment: "right",
+      hidden: false,
+      title: "Monumento a Jaime Garzón",
+      image: "./images/monumento.png",
+      description:
+        "En la localidad de Teusaquillo, en el Barrio Quinta Paredes, se encuentra ubicado el monumento a Jaime Garzón a pocos metros del lugar en donde fue asesinado. Esta escultura representa la dimensión de mediador y constructor de paz de Jaime Garzón capturando un momento icónico ante la opinión pública cuando acompañó un proceso de liberación de secuestrados y ondeó la bandera de Colombia junto a un helicóptero 11 . Esta iniciativa colectiva estuvo acompañada también por CARACOL y no tuvo financiación pública. El escultor Alejandro Hernández fue el encargado del diseño de este monumento y el de Heriberto de la Calle.",
+      location: {
+        center: [-74.07658339999999, 4.619240467762259], //4.619240467762259, -74.07658339999999
+        zoom: 19,
+        pitch: 70,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
+      id: "cap08",
+      alignment: "left",
+      hidden: false,
+      title: "Colegio Jaime Garzón, Britalia",
+      image: "./images/colegioBritalia.png",
+      description:
+        "La conmemoración de Jaime Garzón, a través de la dedicación del colegio en la localidad de Kennedy en su memoria, representa un acto de justicia histórica y reconocimiento a su legado. Jaime no solo fue una voz crítica y satírica que desafió los abusos de poder y la corrupción en Colombia, sino que también desempeñó un rol crucial como alcalde de la localidad de Sumapaz. Su trabajo en esta región, caracterizada por su historia de conflicto armado, se destacó por su compromiso con la paz y el desarrollo social. Nombrar instituciones educativas en su honor no solo preserva su memoria, sino que también inspira a las nuevas generaciones a seguir luchando por una sociedad más justa y equitativa, basada en los valores de la paz y la reconciliación que Jaime defendió hasta su asesinato.",
+      location: {
+        center: [-74.17548195767148, 4.626048371613873], //4.626048371613873, -74.17548195767148
+        zoom: 19,
+        pitch: 70,
+        bearing: 0,
+      },
+      mapAnimation: "flyTo",
+      rotateAnimation: false,
+      callback: "",
+      onChapterEnter: [],
+      onChapterExit: [],
+    },
+    {
+      id: "cap09",
+      alignment: "right",
+      hidden: false,
+      title: "Colegio Campestre Jaime Garzón,",
+      image: "./images/colegioCampestre.png",
+      description:
+        "El Colegio Campestre Jaime Garzón, abre sus puertas a la educación el 6 de junio de 1996, en ese momento se llamó Unidad Básica y Agropecuaria Las Auras y ofrecía la educación Básica Secundaria hasta el grado 9°, en el año 2001 fue autorizada por la Secretaría de Educación del Distrito para ofrecer la educación media académica. Luego de un proceso de integración que se dio en el año 2003, se integró con los 13 Centros Educativos Rurales de los corregimientos de Nazareth y Betania, adoptando el nombre de Institución Educativa Distrital Rural Las Auras y Otras. Posteriormente por incluir el vocablo COLEGIO e identidad institucional mediante un proceso de participación con la comunidad educativa por acuerdo del Consejo Directivo adopta el nombre de COLEGIO CAMPESTRE JAIME GARZÓN I.E.D. (RedAcadémica, SED, 2021)",
+      location: {
+        center: [-74.17221324232851, 4.1708950092991754], //4.1708950092991754, -74.17221324232851
         zoom: 19,
         pitch: 70,
         bearing: 0,
